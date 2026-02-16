a93be8f9aa6c91242f660ce9ee0d3adffb75d171
bonhart <lozsek.peter@gmail.com>
2026-02-16 17:28:17 +0100

    update

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.go" },
 
     -- import/override with your plugins
     { import = "plugins" },
